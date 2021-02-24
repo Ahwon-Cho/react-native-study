0cf31a6de1e21195bab59a9ffb2f59f0625e20f7
Ahwon Cho <awony82@gmail.com>
2021-02-24 00:16:57 -0800

    added style

--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -1,15 +1,29 @@
 import React from 'react';
-import { View, Text, StyleSheet} from 'react-native';
+import { View, Text, StyleSheet, TextInput} from 'react-native';
 
 const TextScreen = () => {
+    const name = "";
     return (
     <View>
         <Text>Text Screen</Text>
+        <TextInput 
+        style = {styles.input} 
+        autoCapitalize="none"
+        value={name}
+        autoCorrect={false}
+        />
+        <Text>Your name is {name}</Text>
     </View>
     )
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    input: {
+        margin: 15,
+        borderColor: 'black',
+        borderWidth: 1 
+    }
+});
 
 export default TextScreen;
 
